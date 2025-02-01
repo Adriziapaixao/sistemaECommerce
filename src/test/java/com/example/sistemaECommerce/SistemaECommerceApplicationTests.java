b9e6ab7db1ed214cadf04add947d4fc595613522
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-01-31 21:28:02 -0300

    cria repositório do projeto

---
+++ src/test/java/com/example/sistemaECommerce/SistemaECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemaECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
