68bb9d3d30f0e35a4151b9b41e94bfa689ac17c5
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-02-02 16:44:35 -0300

    Implementa e ajusta a classe ClienteController: - Adicionado endpoint para cadastrar cliente. - Adicionado endpoint para buscar cliente por CPF. - Adicionado endpoint para atualizar dados do cliente por CPF. - Adicionado endpoint para listar todos os clientes. - Melhorias gerais na estrutura e validação de dados.

---
+++ src/main/java/com/example/sistemaECommerce/controllers/ClienteController.java
@@ -0,0 +1,58 @@
+package com.example.sistemaECommerce.controllers;
+
+import com.example.sistemaECommerce.dtos.ClienteDTO;
+import com.example.sistemaECommerce.models.ClienteEntity;
+import com.example.sistemaECommerce.repositories.ClienteRepository;
+import com.example.sistemaECommerce.services.ClienteService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+@Validated
+public class ClienteController {
+
+    @Autowired
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ClienteEntity> cadastrarCliente(@Validated @RequestBody ClienteDTO clienteDTO) {
+    ClienteEntity cadastrarClente = clienteService.cadastrarCliente(clienteDTO);
+    return ResponseEntity.ok(cadastrarClente);
+    }
+
+    @GetMapping("/{cpf}")
+    public ResponseEntity<ClienteEntity> getClienteByCpf(@PathVariable String cpf) {
+        ClienteEntity cliente = clienteService.getClienteByCpf(cpf);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PutMapping("/{cpf}")
+    public ResponseEntity<ClienteEntity> atualizarCliente(
+         @PathVariable String cpf,
+         @Valid @RequestBody ClienteDTO clienteDTO) {
+        ClienteEntity clienteAtualizado = clienteService.atualizaDadosCliente(cpf, clienteDTO.toEntity());
+        return ResponseEntity.ok(clienteAtualizado);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteEntity>> listarClientes() {
+        List<ClienteEntity> clientes = clienteService.listarTodosClientes();
+        return ResponseEntity.ok(clientes);
+    }
+
+
+}
+
+
